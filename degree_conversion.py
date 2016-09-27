d03bd361889b24c8a66bbe700413cc6678e9ca6c
Autumn Coleman <autumn.eve.coleman@gmail.com>
2016-09-27 12:10:18 -0400

    Completed function exercises and added degree conversion 2 with circle plot

--- degree_conversion.py
+++ degree_conversion.py
@@ -1,10 +1,10 @@
 import matplotlib.pyplot as plot
-C = float(raw_input("Temperature in C? " ))
+celcius = float(raw_input("Temperature in C? " ))
 
 def fahrenheit(celcius):
     return (celcius * 1.8) + 32.0
 
-xs = range(-100, 111)
+xs = range(-10, 40)
 ys = []
 for x in xs:
     ys.append(fahrenheit(x))
